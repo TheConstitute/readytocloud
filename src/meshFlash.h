b4c124ed102252dfb0bd44e1085ec3ce47723578
achimm <theconstitute@ReadyToCloud.local>
2013-08-30 17:52:26 +0200

    see .cpp comment

--- src/meshFlash.h
+++ src/meshFlash.h
@@ -18,20 +18,36 @@ public:
 
     meshFlash();
 
+    void setColor(ofColor &color);
+    
     void create(ofVec3f start, ofVec3f end, float amplitude);
     
-    void update(float deltaTime);
+    //void update(float deltaTime);
     
     void draw();
 
+    inline bool isActive()
+    {
+        return is_active;
+    }
     
 private:
 
     void getPerpendicular(const ofVec3f &vec, ofVec3f &pv);
     
-    int iterations = 6;
+    const int iterations = 6;
+    
+    float creationTime;
+    
+    const float max_live_time = 0.5f;
+    const float min_live_time = 0.15f;
+    
+    float live_time;
+    
+    bool is_active;
     
     ofMesh mesh;
 
+    ofColor flash_color;
 };
 #endif
